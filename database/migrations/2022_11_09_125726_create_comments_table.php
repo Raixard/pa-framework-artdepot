a9d8d64bc9139407c65b23e706acd468bc3e601b
Gial88 <muhammad.basith18@gmail.com>
2022-11-09 21:06:39 +0800

    update migration user,creations, comments, likes | Gial

--- database/migrations/2022_11_09_125726_create_comments_table.php
+++ database/migrations/2022_11_09_125726_create_comments_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('profile_image');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('creation_id')->constrained('creations')->cascadeOnDelete();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
